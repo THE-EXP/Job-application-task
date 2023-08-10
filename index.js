bc817fed6fe9638976fd09a3b9597b18eb9cdec6
THE_EXP <105677206+THE-EXP@users.noreply.github.com>
2023-08-10 16:25:09 +0300

    Fix it all!
    Finally fix 2 problems with one stone, update .env to accommodate the changes

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 require('dotenv').config('./');
 app.set(express.urlencoded({extended: true}));
 
-const queue = process.env.BASE_QUEUE || 'TaskQueue';
+
+
 const mainPort = process.env.MAIN_SERVER_PORT || 8000;
 const RabbitMQURL = process.env.RMQ_URL || 'amqp://localhost:5672';
 
 async function sendMessage(req, res){
 	var message = req.query.msg
+	const queue = process.env.BASE_QUEUE_A || 'Forth';
 	var connection = await amqp.connect(RabbitMQURL);
 	console.log('connection opened');
 	var channel = await connection.createChannel();
@@ -27,6 +29,7 @@ async function sendMessage(req, res){
 }
 
 async function getMessage(req, res){
+	const queue = process.env.BASE_QUEUE_B || 'Back';
 	var connection = await amqp.connect(RabbitMQURL);
 	console.log('connection opened');
 	var channel = await connection.createChannel();
@@ -45,14 +48,18 @@ async function getMessage(req, res){
 		console.log(`Received: ${msg.content.toString()}`);
 		channel.ack(msg);
 	}, {noAck: false, consumerTag: 'sender'});
+	setTimeout(() => {
+		channel.close();
+    connection.close();
+		console.log('Connection closed')
+	},  500);
 }
 
-app.post('/add', (req, res) => {
-	sendMessage(req, res);
-	getMessage(req, res);
+app.post('/add', async (req, res) => {
+		await sendMessage(req, res);
+		await getMessage(req, res);
 	/** 
-	 * ! Crashes after 2 requests, reason unknown, Node says because i'm trying to set headers when they were already sent to the client, How?
-	 * TODO: fix this
+	 * * Temporary fix: use 2 queues to separate the send/receive sides so that they don't get their own messages, seems to have fixed both hears problem and the self-read messages
 	 **/
 });
 
--- worker.js
+++ worker.js
@@ -7,6 +7,7 @@ const RabbitMQURL = process.env.RMQ_URL || 'amqp://localhost:5672';
 start();
 
 async function start() {
+  const queue = process.env.BASE_QUEUE_A || 'Forth';
   var connection = await amqp.connect(RabbitMQURL);
 	console.log(`Opened connection`)
   var channel = await connection.createChannel();
@@ -16,18 +17,16 @@ async function start() {
 	
   console.log(` [*] Waiting for messages in ${queue}. To exit press CTRL+C`);
   channel.consume(queue, async (msg) => {
+    const queue = process.env.BASE_QUEUE_B || 'Back';
+    channel.ack(msg);
     if(msg.content.toString() == 'Hello, World!') {
-			console.log(`Sending message: "Hello there!"`);
+			console.log(`Sending message: "Hello there!", to ${queue}`);
       channel.sendToQueue(queue, Buffer.from('Hello there!'));
     } else if(msg.content.toString() == 'Hello, World! It is I The Postman!') {
-			console.log(`Sending message: "Hello there, Postman!"`);
+			console.log(`Sending message: "Hello there, Postman!", to ${queue}`);
 			channel.sendToQueue(queue, Buffer.from('Hello there, Postman!'));
     }
-		//channel.ack(msg); 
-		/**Seems to receive it's own message with acknowledgements enabled?
-		** strangely, main microservice(index.js) works with acknowledgments just fine, what the hell??
-		* TODO: find a fix for this
-		**/
+		
 		console.log(`Received: ${msg.content.toString()}`);
-    }, {noAck: true, consumerTag: 'consumer'});
+    }, {noAck: false, consumerTag: 'consumer'});
 }
